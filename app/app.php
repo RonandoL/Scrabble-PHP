c323cfc5b712a2409e22a82803b2a5145a04539b
Jason Awbrey and Ronando Long <jason.s.awbrey@gmail.com>
2016-02-18 09:53:44 -0800

    Add fixed syntax, MVP working

--- app/app.php
+++ app/app.php
@@ -3,24 +3,26 @@
 	require_once __DIR__.'/../vendor/autoload.php';
 	require_once __DIR__.'/../src/Scrabble.php';
 
+// use Symfony\Component\Debug\Debug;
+// Debug::enable();
+
+
 	$app = new Silex\Application();
+// $app['debug'] = true;
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
 	$app->get('/', function() use ($app)
 	{
 		return $app['twig']->render('scrabble.hmtl.twig');
-		
+
 	});
 
-	// User input form
-	$app->get('/results', function() use ($app)
-	{
+
+	$app->get('/results', function() use ($app)	{
 		$scrabble = new Scrabble($_GET['word']);
-		return $app['twig']->render('scrabble.hmtl.twig', array('scrabble' => $scrabble);
-
+		return $app['twig']->render('scrabble.hmtl.twig', array('scrabble' => $scrabble));
 	});
-
 
 	return $app;
 
