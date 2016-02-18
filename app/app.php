c53e034fb79efaa545a7870689e688700a04e006
Jason Awbrey and Ronando Long <ronando.long@gmail.com>
2016-02-18 09:45:54 -0800

    Add all functionality, currently a parse error

--- app/app.php
+++ app/app.php
@@ -1,13 +1,26 @@
 <?php
 
 	require_once __DIR__.'/../vendor/autoload.php';
-	require_once __DIR__.'/../src/Scrabble.php'; 
+	require_once __DIR__.'/../src/Scrabble.php';
 
 	$app = new Silex\Application();
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-	$app->get('/', function(){return 'Hello, World!';});
+	$app->get('/', function() use ($app)
+	{
+		return $app['twig']->render('scrabble.hmtl.twig');
+		
+	});
+
+	// User input form
+	$app->get('/results', function() use ($app)
+	{
+		$scrabble = new Scrabble($_GET['word']);
+		return $app['twig']->render('scrabble.hmtl.twig', array('scrabble' => $scrabble);
+
+	});
+
 
 	return $app;
 
